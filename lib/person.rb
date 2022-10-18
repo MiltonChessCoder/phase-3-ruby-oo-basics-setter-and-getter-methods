bdd08bc06d7db5073e8847bc89bcd045445bd615
Milton <miltonsoftwareengineer@gmail.com>
2022-10-18 12:29:59 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,36 @@
 class Person
+    attr_reader :name
+    attr_writer :name
+    attr_reader :job
+    
+    attr_writer :job
+    # setter method
+
+    def name=(person_name)
+        @name=person_name
+    end
+    #getter method
+    def name
+        @name
+    end
+    
+    # def name=(new_name)
+    #     @name =name
+    # end
+
+    
+
+    #setter method
+    def job=(person_job)
+        @job=(person_job)
+    end
+    
+    #getter method
+    def job
+        @job
+    end
+
 
 end
+
+
